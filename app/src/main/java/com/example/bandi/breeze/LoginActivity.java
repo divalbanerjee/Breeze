6814f38ea41ca12d8eb724a8d8ce566e8e1629e4
Dival Banerjee <bandiv2011@gmail.com>
2018-02-24 23:40:32 -0500

    defined components in LoginActivity.java

--- app/src/main/java/com/example/bandi/breeze/LoginActivity.java
+++ app/src/main/java/com/example/bandi/breeze/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.bandi.breeze;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        final TextView btnRegister = (Button) findViewById(R.id.btnSignUp);
+
 
     }
 }
