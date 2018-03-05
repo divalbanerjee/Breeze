7a3d1488e725ca06042613ee5217e40471f75fe7
Dival Banerjee <bandiv2011@gmail.com>
2018-03-05 09:12:08 -0500

    Finished basic login authentication. Added Register Activity

--- app/src/main/java/com/example/bandi/breeze/splashScreenActivity.java
+++ app/src/main/java/com/example/bandi/breeze/splashScreenActivity.java
@@ -1,14 +1,30 @@
 package com.example.bandi.breeze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class splashScreenActivity extends AppCompatActivity {
+
+
+    TextView tvContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        tvContinue = (TextView) findViewById(R.id.tvContinue);
+        final Intent intent = new Intent(this, LoginActivity.class);
+        tvContinue.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
